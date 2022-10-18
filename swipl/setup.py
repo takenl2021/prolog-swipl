7138fa47e33b78656821ab46e0edca4087bc82ac
Tatsuya Katsura <pcn05fd3@s.okayama-u.ac.jp>
2022-10-18 20:29:03 +0900

    add setup.py

---
+++ swipl/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os import path
+# here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
+
+setup(
+    name='swipl',  # Required
+    version='1.0.0',  # Required
+    description='pyswipのProlog',  # Optional
+    author='takenl2021',
+    classifiers=[  # Optional
+        'License :: MIT License',
+    ],
+    install_requires=[
+        'regex'
+    ],
+    packages=['swipl'],  # Required
+)
